ec285053cc36488245c35001541f8b8d29ebd65f
kshtitijkumrawat20 <kshitijk146@gmail.com>
2026-01-31 16:50:15 +0530

    Implement database factory function for PostgreSQL instance creation

--- src/db/factory.py
+++ src/db/factory.py
@@ -1,3 +1,24 @@
 from src.config import get_settings
 
-# from src.db.interface.base import 
+from src.db.interface.base import BaseDatabase
+from src.db.interface.postgresql import PostgreSQLDatabase, PostgresqlSettings
+
+def make_database() -> BaseDatabase:
+    """
+    Factory function to create a database instance based on configuration settings.
+    Returns:
+    Factory function to create a database instance based on configuration settings.
+    """
+
+    settings = get_settings()
+    config = PostgresqlSettings(
+        database_url = settings.postgress_database_url, 
+        echo_sql = settings.postgres_echo_sql,
+        pool_size = settings.postgres_pool_size,
+        max_overflow = settings.postgres_max_overflow
+    )
+    database = PostgreSQLDatabase(config = config)
+    database.startup()
+    return database
+
+
