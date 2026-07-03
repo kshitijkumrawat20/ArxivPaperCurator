40ed816ab7c31dc054f5c71e6762b9bbfbc5847d
kshtitijkumrawat20 <kshitijk146@gmail.com>
2026-07-03 13:42:29 +0530

    added airflow paper ingestion DAGs

--- airflow/dags/hello_world.py
+++ airflow/dags/hello_world.py
@@ -47,9 +47,9 @@ def check_services():
 
 # DAG configuration 
 default_args = {
-    "owner": "airflow",
+    "owner": "rag",
     "depends_on_past": False,
-    "start_date": datetime(2023, 1, 1),
+    "start_date": datetime(2026, 1, 1),
     "retries": 1,
     "email_on_failure": False,
     "email_on_retry": False,
@@ -63,6 +63,7 @@ dag = DAG(
     description="A simple hello world DAG for testing",
     schedule_interval=None,
     catchup=False,
+    tags = ["weekl", "tesing"]
 )
 
 # defining the task 
