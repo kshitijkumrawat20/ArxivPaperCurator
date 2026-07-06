6b84c885f8d684ae54663f3901d5c8753abbedd1
kshtitijkumrawat20 <kshitijk146@gmail.com>
2026-07-06 12:14:29 +0530

    Refactor configuration settings to enhance structure, add OpenSearch settings, and validate PDF cache directory

--- src/config.py
+++ src/config.py
@@ -1,24 +1,48 @@
 
 #Central settings object first. Everything else depends on it.
 
-from typing import List, Union 
+from typing import List, Literal
+import os 
+from pathlib import Path 
 from pydantic import BaseModel, Field, field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict 
 
-class DefaultSettings(BaseSettings):
+PROJECT_ROOT = Path(__file__).parent.parent
+ENV_FILE_PATH = PROJECT_ROOT / ".env"
+
+class BaseConfigSettings(BaseSettings):
     """Default settings for the application."""
     model_config = SettingsConfigDict(
-        env_file=".env",
+        env_file=[".env", str(ENV_FILE_PATH)],
         extra="ignore",
         frozen=True, 
-        env_nested_delimiter="__"
+        env_nested_delimiter="__",
+        case_sensitive=False,
     )
  # adding arxiv API client settings 
 
-class ArxivSettings(DefaultSettings):
+class ArxivSettings(BaseConfigSettings):
     """Arxiv API client settings."""
+
+    model_config = SettingsConfigDict(
+        env_file=[".env", str(ENV_FILE_PATH)],
+        env_prefix="ARXIV__",
+        extra="ignore",
+        frozen=True,
+        case_sensitive=False,
+    )
     
     base_url: str ="https://export.arxiv.org/api/query"
+    pdf_cache_dir: str =".data/arxiv_pdfs"
+    rate_limit_delay: float = 3.0 
+    timeout_seconds: int = 30 
+    max_results: int = 15 
+    search_category: str= "cs.AI"  # Default search category for AI papers
+    download_max_retries: int = 3
+    download_retry_delay: float = 5.0  # Delay between retries in seconds
+    max_concurret_downloads: int = 5  # Maximum number of concurrent downloads
+    max_concurrent_parsing: int = 1  
+
     namespaces: dict = Field(
         default={
             "atom": "http://www.w3.org/2005/Atom",
@@ -26,29 +50,47 @@ class ArxivSettings(DefaultSettings):
             "arxiv": "http://arxiv.org/schemas/atom"
         }
     )
-    pdf_cache_dir: str =".data/arxiv_pdfs"
-    rate_limit_delay: float = 3.0 
-    timeout_seconds: int = 10 
-    max_results: int = 100 
-    search_category: str= "cs.AI"  # Default search category for AI papers
 
-
-class PDFParserSettings(DefaultSettings):
-    """PDF parser settings."""
+    @field_validator("pdf_cache_dir")
+    @classmethod 
+    def validate_pdf_cache_dir(cls, v: str) -> str:
+        """Ensure the PDF cache directory exists."""
+        os.makedirs(v, exist_ok=True)
+        return v
     
+
+class PDFParserSettings(BaseConfigSettings):
+    """PDF parser settings."""
+    model_config = SettingsConfigDict(
+        env_file=[".env", str(ENV_FILE_PATH)],
+        env_prefix="PDF_PARSER__",
+        extra="ignore",
+        frozen=True,
+        case_sensitive=False,
+    )
     max_pages: int = 30
     max_file_size_mb : int =20 
     do_ocr: bool = False 
     do_tables_structure: bool = True 
 
-
-
-class Settings(DefaultSettings):
+class OpenSearchSettings(BaseConfigSettings):
+    """OpenSearch settings."""
+    model_config = SettingsConfigDict(
+        env_file=[".env", str(ENV_FILE_PATH)],
+        env_prefix="OPENSEARCH__",
+        extra="ignore",
+        frozen=True,
+        case_sensitive=False,
+    )
+    host: str = "http://localhost:9200"
+    index_name: str = "arxiv_papers"
+    max_text_size: int = 1000000
+class Settings(BaseConfigSettings):
     """Application settings."""
-
+    
     app_version: str = "0.1.0"  
     debug: bool = True 
-    environment: str = "development"
+    environment: Literal["development","staging","production"] = "development"
     service_name: str = "rag_api"
 
     # PostgreSQL database settings
@@ -57,13 +99,9 @@ class Settings(DefaultSettings):
     postgres_pool_size: int = 20 
     postgres_max_overflow: int = 0
 
-    # openSearch configuration 
-    opensearch_host: str = "http://localhost:9200"
-
     # ollama configurations 
     ollama_host: str = "http://localhost:11434"
-    ollama_model:  List[str] = Field(default = ["llama3.2:1b"])
-    ollama_default_model: str = "llama3.2:1b"
+    ollama_model: str = "llama3.2:1b"
     ollama_timeout : int = 300 # atleast 5 min 
 
     # arXiv settings
@@ -72,13 +110,23 @@ class Settings(DefaultSettings):
     # PDF parser settings
     pdf_parser: PDFParserSettings = Field(default_factory=PDFParserSettings) 
 
-    @field_validator("ollama_model", mode = "before")
+    # OpenSearch settings
+    opensearch: OpenSearchSettings = Field(default_factory=OpenSearchSettings)
+
+    @field_validator("postgres_database_url")
     @classmethod
-    def parse_ollama_model(cls, value):
-        """Parse the ollama_model field to ensure it is a list of strings."""
-        if isinstance(value, str):
-            return [model.strip() for model in value.split(",") if model.strip()]
-        return value 
+    def validate_database_url(cls, v: str) -> str:
+        if not (v.startswith("postgresql://") or v.startswith("postgresql+psycopg2://")):
+            raise ValueError("Invalid PostgreSQL database URL. It must start with 'postgresql://' or 'postgresql+psycopg2://'.")
+        return v
+
+    # @field_validator("ollama_model")
+    # @classmethod
+    # def parse_ollama_model(cls, value):
+    #     """Parse the ollama_model field to ensure it is a list of strings."""
+    #     if isinstance(value, str):
+    #         return [model.strip() for model in value.split(",") if model.strip()]
+    #     return value 
     
 def get_settings() -> Settings:
     """Get the application settings class instance."""
